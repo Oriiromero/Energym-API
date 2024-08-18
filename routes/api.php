58a802bc08bfaa411f72599639f94df3b37a199c
oriana <oriana.nahir.r@gmail.com>
2024-08-18 14:28:58 +0200

    adding resources and requests

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,15 @@ use App\Http\Controllers\TrainerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
-
+//Trainers
 Route::get('trainers', [TrainerController::class, 'index']);
 Route::get('trainers/{trainer}', [TrainerController::class, 'show']);
 Route::post('trainers', [TrainerController::class, 'store']);
 Route::put('trainers/{trainer}', [TrainerController::class, 'update']);
 Route::patch('trainers/{trainer}', [TrainerController::class, 'update']);
 Route::delete('trainers/{trainer}', [TrainerController::class, 'destroy']);
+
+//Activities
+//Bookings
+//Payments
+//Subscriptions
